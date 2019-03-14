438b083fbff06549cb8afb8db91a7e081cd8905d
oracle <oracle@inge.itam.mx>
2019-03-14 13:19:41 -0600

    Change size Form1

--- Tarea5/Tarea5/FuncYProc.Designer.cs
+++ Tarea5/Tarea5/FuncYProc.Designer.cs
@@ -30,7 +30,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(73, 195);
+            this.button1.Location = new System.Drawing.Point(24, 105);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(119, 42);
             this.button1.TabIndex = 0;
@@ -40,7 +40,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(280, 195);
+            this.button2.Location = new System.Drawing.Point(182, 105);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(119, 42);
             this.button2.TabIndex = 1;
@@ -51,9 +51,9 @@
             // cboOrg
             // 
             this.cboOrg.FormattingEnabled = true;
-            this.cboOrg.Location = new System.Drawing.Point(130, 64);
+            this.cboOrg.Location = new System.Drawing.Point(24, 64);
             this.cboOrg.Name = "cboOrg";
-            this.cboOrg.Size = new System.Drawing.Size(209, 21);
+            this.cboOrg.Size = new System.Drawing.Size(277, 21);
             this.cboOrg.TabIndex = 2;
             // 
             // FuncYProc
@@ -62,7 +62,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(492, 396);
+            this.ClientSize = new System.Drawing.Size(324, 205);
             this.Controls.Add(this.cboOrg);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
