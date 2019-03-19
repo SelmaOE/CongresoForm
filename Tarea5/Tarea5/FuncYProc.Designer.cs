cbf14ee807eae1a5ede24c3c0f215ba9b89486c6
oracle <oracle@inge.itam.mx>
2019-03-19 13:11:45 -0600

    added procedure to funcyproc

--- Tarea5/Tarea5/FuncYProc.Designer.cs
+++ Tarea5/Tarea5/FuncYProc.Designer.cs
@@ -26,11 +26,18 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.cboOrg = new System.Windows.Forms.ComboBox();
+            this.reslabel = new System.Windows.Forms.Label();
+            this.cboOrg2 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.addorgBtn = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(24, 105);
+            this.button1.Location = new System.Drawing.Point(24, 126);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(119, 42);
             this.button1.TabIndex = 0;
@@ -40,7 +47,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(182, 105);
+            this.button2.Location = new System.Drawing.Point(182, 126);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(119, 42);
             this.button2.TabIndex = 1;
@@ -51,10 +58,77 @@
             // cboOrg
             // 
             this.cboOrg.FormattingEnabled = true;
-            this.cboOrg.Location = new System.Drawing.Point(24, 64);
+            this.cboOrg.Location = new System.Drawing.Point(98, 43);
             this.cboOrg.Name = "cboOrg";
-            this.cboOrg.Size = new System.Drawing.Size(277, 21);
+            this.cboOrg.Size = new System.Drawing.Size(203, 21);
             this.cboOrg.TabIndex = 2;
+            this.cboOrg.Text = "Nombre de la Organización:";
+            // 
+            // reslabel
+            // 
+            this.reslabel.AutoSize = true;
+            this.reslabel.Location = new System.Drawing.Point(95, 185);
+            this.reslabel.Name = "reslabel";
+            this.reslabel.Size = new System.Drawing.Size(0, 13);
+            this.reslabel.TabIndex = 3;
+            this.reslabel.Visible = false;
+            // 
+            // cboOrg2
+            // 
+            this.cboOrg2.FormattingEnabled = true;
+            this.cboOrg2.Location = new System.Drawing.Point(98, 99);
+            this.cboOrg2.Name = "cboOrg2";
+            this.cboOrg2.Size = new System.Drawing.Size(203, 21);
+            this.cboOrg2.TabIndex = 4;
+            this.cboOrg2.Text = "Nombre de la Organización:";
+            this.cboOrg2.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Organización 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Organización 2";
+            this.label2.Visible = false;
+            // 
+            // addorgBtn
+            // 
+            this.addorgBtn.Location = new System.Drawing.Point(98, 70);
+            this.addorgBtn.Name = "addorgBtn";
+            this.addorgBtn.Size = new System.Drawing.Size(133, 23);
+            this.addorgBtn.TabIndex = 7;
+            this.addorgBtn.Text = "Agregar organización";
+            this.addorgBtn.UseVisualStyleBackColor = true;
+            this.addorgBtn.Click += new System.EventHandler(this.addorgBtn_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "----------------------";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(233, 76);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "----------------------";
             // 
             // FuncYProc
             // 
@@ -63,6 +137,13 @@
             this.BackColor = System.Drawing.SystemColors.Control;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(324, 205);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.addorgBtn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cboOrg2);
+            this.Controls.Add(this.reslabel);
             this.Controls.Add(this.cboOrg);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -70,6 +151,7 @@
             this.Text = "Funciones y Procedimientos de la BD";
             this.Load += new System.EventHandler(this.FuncYProc_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -78,5 +160,12 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.ComboBox cboOrg;
+        private System.Windows.Forms.Label reslabel;
+        private System.Windows.Forms.ComboBox cboOrg2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button addorgBtn;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
--- Tarea5/Tarea5/FuncYProc.cs
+++ Tarea5/Tarea5/FuncYProc.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.OleDb;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -16,10 +17,14 @@ namespace Tarea5 {
         string cadSql;
         Varios.Comunes comunes = new Varios.Comunes();
 
+        //Variable para conectarse a Oracle.
+        OleDbConnection cnOracle;
+
         public FuncYProc() {
             InitializeComponent();
         }
         private void FuncYProc_Load(object sender, EventArgs e) {
+
             //1- Hace la conexìón a la BD de Oracle.
             GestorBD = new GestorBD.GestorBD("MSDAORA", "bd01", "linesp", "oracle");
 
@@ -27,16 +32,80 @@ namespace Tarea5 {
             cadSql = "select * from T4Organizacion";
             GestorBD.consBD(cadSql, dsOrg, "T4Organizacion");
             comunes.cargaCombo(cboOrg, dsOrg, "T4Organizacion", "Nombre");
+            //Lo mismo para el segundo combobox.
+            comunes.cargaCombo(cboOrg2,dsOrg, "T4Organizacion", "Nombre");
+
+
         }
 
         private void button1_Click(object sender, EventArgs e) {
+            OleDbCommand procOrg;
+            OleDbParameter par;
+            string nomOrg;
+            int tot;
+
+            // 1 - Abrir la conexión a la BD.
+            cnOracle = new OleDbConnection("Provider=MSDAORA; Data Source=oracle.itam.mx;" +
+            "User ID=bd01;Password=linesp");
+            cnOracle.Open();
+            procOrg = new OleDbCommand();
+            procOrg.Connection = cnOracle;
+
+            //2- Especificar el llamado al procedimiento  (en general: al subprograma).
+            procOrg.CommandText = "T4E"; //nombre del procedimiento
+            procOrg.CommandType = CommandType.StoredProcedure; //tipo del procedimiento. Store precedure puede ejecutar procedimientos y funciones
+
+            //3- Especificar los parámetros:
+            // a) Nota: primero hay que definir el tipo de valor que regresará la función.
+            par = new OleDbParameter("RETURN_VALUE", OleDbType.Integer,
+              4, ParameterDirection.ReturnValue, false, 4, 0, "total", DataRowVersion.Current, 0);
+            procOrg.Parameters.Add(par);
+
+            // b) luego hay que definir los parámetros de entrada (uno en este caso).
+            nomOrg = Convert.ToString(cboOrg.SelectedItem);
+            par = new OleDbParameter("nombreO", nomOrg);
+            procOrg.Parameters.Add(par);
+
+            //4- Ejecutar el procedimiento (en general: el subprograma).
+            try
+            {
+                procOrg.ExecuteNonQuery();
+
+                //5- Recuperar el valor regresado por la función.
+                tot = Convert.ToInt16(procOrg.Parameters["RETURN_VALUE"].Value);
+                MessageBox.Show("Ha participado en: " + tot);
+            }
+            catch (OleDbException err)
+            {
+                MessageBox.Show(err.Message);
+            }
+
+            //6- Cerrar la conexión a la BD.
+            cnOracle.Close();
 
         }
 
         
 
         private void button2_Click(object sender, EventArgs e) {
+            DataSet dsTemp = new DataSet();
+            DataRow fila;
 
+            cadSql = "declare Nom1 varchar(20); Nom2 varchar(20); m varchar(20);" +
+                    "begin Nom1:= '" + cboOrg.SelectedItem + "'; Nom2:= '" + cboOrg2.SelectedItem + "';" +
+                     "T4f(Nom1, Nom2, m); dbms_output.put_line(m); end;";
+            GestorBD.consBD(cadSql, dsTemp, "Temp");
+            fila = dsTemp.Tables["Temp"].Rows[1];
+            reslabel.Text = "Ha participado más: "+fila.ToString();
+            reslabel.Visible = true;
+        }
+
+
+
+        private void addorgBtn_Click(object sender, EventArgs e)
+        {
+            label2.Visible = true;
+            cboOrg2.Visible = true;
         }
     }
 }
