d5a7c385df9bdceee313450991bcaac5e6ebb950
oracle <oracle@inge.itam.mx>
2019-04-04 14:25:11 -0600

    added transaction form

---
+++ Tarea5/Tarea5/Transaccion.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tarea5
+{
+    public partial class Transaccion : Form
+    {
+        public Transaccion()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtHor_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cboCongreso_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateCong_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cboArticulo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cboAutor_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
